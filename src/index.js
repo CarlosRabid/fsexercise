5d80bbba96a58e40e07558a6ced55542c7631dd1
Carlos Cornejo <carlitos.cornejo.n@hotmail.com>
2020-11-19 22:38:08 -0500

    deploying

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+const express = require("express");
 const port = process.env.PORT || 5000;
-const path = require('path')  
+const path = require('path')
+const app = express();  
 
 ReactDOM.render(
   <React.StrictMode>
@@ -12,7 +14,9 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
